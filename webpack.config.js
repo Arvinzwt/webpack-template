2f69c27e25ccff56aa64d46b7fde1ff638f6b188
Arvinzwt <zhangwentao577614@gmail.com>
2020-11-19 18:19:10 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin({
             cleanAfterEveryBuildPatterns: ['dist']
