41119bb4763e57110bc6ecaf142de6ba587c7f59
Arvinzwt <zhangwentao577614@gmail.com>
2020-11-20 11:39:57 +0800

    模块热替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
-    devtool: 'inline-source-map',
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist']
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
-    ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    }
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',// 错误跟踪
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        // html设定插件
+        new CleanWebpackPlugin(['dist']),
+
+        //html清理插件
+        new HtmlWebpackPlugin({
+            title: "page"
+        }),
+
+        // 热模块替换
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
 };
