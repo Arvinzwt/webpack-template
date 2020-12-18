46587ae60728de74b9d3432452fee352ac9ff694
Arvinzwt <zhangwentao577614@gmail.com>
2020-12-18 16:49:09 +0800

    ad

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Socket from './socket'
-import User from './User';
-import UserList from './UserList';
-import Teacher from './teacher'
-// vuex-persistedstate默认持久化所有state，指定需要持久化的state
-import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
+
 export default new Vuex.Store({
-    modules: {
-        Socket,
-        User,
-        UserList,
-        Teacher,
-    }
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,13 @@
 <template>
-    <div class="jr-home">
-        <el-header class="jr-home-header" height="50px">
-            <router-link to="/">{{ $t('home') }}</router-link>
-            <span class="p-1">|</span>
-            <router-link to="/about">{{ $t('about') }}</router-link>
-            <span class="p-1">|</span>
-            <router-link to="/1v1">1v1</router-link>
-            <span class="p-1">|</span>
-            <router-link to="/1v4">1v4</router-link>
-            <debugger-popover class="right" @locale="localeChangeHandler"></debugger-popover>
-        </el-header>
-        <el-row>
-            <img class="logo" :src="$t('logo')" alt="logo"/>
-        </el-row>
-        <help-template :msg="$t('message')"></help-template>
-    </div>
+  <div class="home">
+
+  </div>
 </template>
 
-<script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
-import HelpTemplate from "./../components/Help.vue";
-import DebuggerPopover from '@/components/DebuggerPopover.vue';
+<script>
 
+export default {
+  name: 'Home',
 
-@Component({
-    name: "Home",
-    components: {HelpTemplate,DebuggerPopover},
-})
-
-export default class Home extends Vue {
-    /**
-     * @desc 判定当前语言
-     * @param lang[any]
-     */
-    private localeChangeHandler(lang: any) {
-        if (this.$i18n) {
-            this.$i18n.locale = lang || process.env.VUE_APP_I18N_LOCALE;
-        }
-    }
 }
 </script>
-<style lang="scss" scoped>
-.jr-home {
-    flex: 1;
-    text-align: center;
-
-    .jr-home-header{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        a{
-            color: #fafafa;
-        }
-
-        .right{
-            position: absolute;
-            right: 0;
-            top: 50%;
-            margin-top: -20px;
-        }
-    }
-}
-</style>
